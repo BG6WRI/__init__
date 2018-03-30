004d9027cb1b43a028362ad97206d756350b9c43
BG6WRI <bg6wri@gmail.com>
2018-03-30 10:07:22 +0800

    Nothing important.

---
+++ init.sh
@@ -0,0 +1,3 @@
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get install gcc git cmake vim python pip
---
+++ sdr.sh
@@ -0,0 +1,1 @@
+sudo apt-get install hackrf libhackrf-dev libhackrf0 gnuradio-dev gr-osmosdr gqrx-sdr rtl-sdr gnuradio
