75a187c3f37b3ea9fcc41091b526433c37c8bc9c
BG6WRI <bg6wri@gmail.com>
2019-01-03 07:59:18 +0800

    Update pm3.sh

--- pm3.sh
+++ pm3.sh
@@ -1,5 +1,5 @@
 sudo apt-get install p7zip git build-essential libreadline5 libreadline-dev libusb-0.1-4 libusb-dev libqt4-dev perl pkg-config wget libncurses5-dev gcc-arm-none-eabi libstdc++-arm-none-eabi-newlib
-git clone https://github.com/proxmark/proxmark3.git ~
+git clone https://www.github.com/proxmark/proxmark3.git ~
 cd proxmark3
 sudo cp -rf driver/77-mm-usb-device-blacklist.rules /etc/udev/rules.d/77-mm-usb-device-blacklist.rules
 sudo udevadm control --reload-rules
