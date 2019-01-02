5cf5b2ec8ac8f27ec5c3f57301ec22ddcf0b0326
BG6WRI <bg6wri@gmail.com>
2019-01-03 07:57:42 +0800

    Update sdr.sh

--- sdr.sh
+++ sdr.sh
@@ -1,1 +1,1 @@
-sudo apt-get install hackrf libhackrf-dev libhackrf0 gnuradio-dev gr-osmosdr gqrx-sdr rtl-sdr gnuradio
+sudo apt-get install hackrf libhackrf-dev libhackrf0 gqrx-sdr rtl-sdr pyrtlsdr
