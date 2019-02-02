2579382820b105ffd8620401943e18d01d7a6a75
BG6WRI <bg6wri@gmail.com>
2019-02-03 00:21:17 +0800

    Update hackrf.sh

--- hackrf.sh
+++ hackrf.sh
@@ -1,1 +1,2 @@
-sudo apt-get install hackrf libhackrf-dev libhackrf0 gqrx-sdr rtl-sdr pyrtlsdr gnuradio
+sudo apt-get install hackrf libhackrf-dev libhackrf0 gqrx-sdr rtl-sdr gnuradio
+sudo pip install pyrtlsdr
